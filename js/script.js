9a0a11d107c106eace7b1b765cf501090ff0e8d6
Kyle Crawford <kcraw217@gmail.com>
2023-03-24 14:34:44 -0700

    WIP functions stil

--- js/script.js
+++ js/script.js
@@ -9,21 +9,36 @@ function showResult(language) {
   document.getElementById(language).removeAttribute('class', 'hidden');
 }
 
-window.onload = function() {
-  function doTheThing(event) {
-    let form = document.querySelector("form");
-    form.addEventListener('submit', function()) {
-      epreventDefault();
-      hideResults();
-      const userName = document.getElementById('userName').value;
-      const purpose = document.getElementById('purpose').value;
-      const identify = document.getElementById('identify').value;
-      const date = document.getElementById('date').value;
-      const color = document.getElementById('color').value;
-
-      function name() {
-        document.getElementById('name').innerHTML = 'userName';
-      }
-    }
-  }  
+function name(enter) {
+  document.getElementById('name').innerHTML = enter;
 }
+
+function doTheThing(event) {
+  event.preventDefault();
+  console.log(event);
+  hideResults();
+  const userName = document.getElementById('userName').value;
+  const purpose = document.getElementById('purpose').value;
+  const identify = document.getElementById('identify').value;
+
+  if (identify === 'steve') {
+    showResult("swift");
+  } else if (purpose === 'personal' && identify === 'steve') {
+    showResult('swift');
+  } else if (purpose === 'personal') {
+    showResult('js');
+  } else if (purpose === 'ai'){
+    showResult('python');
+  } else if (purpose === 'new' && identify === 'beyonce') {
+    showResult('js');
+  } else if (purpose === 'new') {
+    showResult('csharp');
+  }
+
+  
+}
+
+window.addEventListener('load', function() {
+  const form = document.getElementById('quiz');
+  form.addEventListener('submit', doTheThing);
+});
